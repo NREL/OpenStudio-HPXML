f7af112e2d7f7163034c18172ab9b7a0b5d02363
Scott Horowitz <scott.horowitz@nrel.gov>
2018-11-21 14:37:58 -0700

    Initial code for Attics/Roofs.

--- resources/EPvalidator.rb
+++ resources/EPvalidator.rb
@@ -84,7 +84,7 @@ class EnergyPlusValidator
 
       # [Attic]
       '/HPXML/Building/BuildingDetails/Enclosure/AtticAndRoof/Attics/Attic' => {
-        '[AtticType="unvented attic" or AtticType="vented attic" or AtticType="flat roof" or AtticType="cathedral ceiling" or AtticType="cape cod"]' => one, # See [AtticType=Unvented] or [AtticType=Vented] or [AtticType=Cape]
+        '[AtticType="unvented attic" or AtticType="vented attic" or AtticType="flat roof" or AtticType="cathedral ceiling" or AtticType="cape cod"]' => one, # See [AtticType=Unvented] or [AtticType=Vented]
         'Roofs' => one, # See [AtticRoof]
         'Walls' => zero_or_one, # See [AtticWall]
       },
@@ -98,11 +98,6 @@ class EnergyPlusValidator
       '/HPXML/Building/BuildingDetails/Enclosure/AtticAndRoof/Attics/Attic[AtticType="vented attic"]' => {
         'Floors' => one, # See [AtticFloor]
         'extension/AtticSpecificLeakageArea' => zero_or_one, # Uses ERI Reference Home if not provided
-      },
-
-      ## [AtticType=Cape]
-      '/HPXML/Building/BuildingDetails/Enclosure/AtticAndRoof/Attics/Attic[AtticType="cape cod"]' => {
-        'Floors' => one, # See [AtticFloor]
       },
 
       ## [AtticRoof]
