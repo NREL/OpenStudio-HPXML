8ddca64bda6694806e4bf3f410e5696289c0f4a9
Scott Horowitz <scott.horowitz@nrel.gov>
2019-01-31 20:53:11 -0700

    Bugfix.

--- resources/EPvalidator.rb
+++ resources/EPvalidator.rb
@@ -104,7 +104,7 @@ class EnergyPlusValidator
       '/HPXML/Building/BuildingDetails/Enclosure/Attics/Attic' => {
         '[AtticType="unvented attic" or AtticType="vented attic" or AtticType="flat roof" or AtticType="cathedral ceiling" or AtticType="cape cod"]' => one, # See [AtticType=Unvented] or [AtticType=Vented] or [AtticType=Cape]
         'Roofs/Roof' => one_or_more, # See [AtticRoof]
-        'Walls/Wall' => zero_or_one, # See [AtticWall]
+        'Walls/Wall' => zero_or_more, # See [AtticWall]
       },
 
       ## [AtticType=Unvented]
