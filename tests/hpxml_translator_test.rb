68935444696274905175b3df97df53f1b576a152
Scott Horowitz <scott.horowitz@nrel.gov>
2019-06-03 11:35:26 -0600

    Squashed 'measures/HPXMLtoOpenStudio/' changes from e23cbfb0..ed755425
    ed755425 Update test.
    9c2d5ffc Update a couple tests.
    464b4c40 Updates code to latest OS-HPXML. Converted API HPXML files to latest HPXML v3 format using a rake task.
    12711f90 Merge commit '4bbf2b94b96e6bfa798d98a2eadebf7247aec98b' into sync
    8f7dcca4 Merge commit '15741939ca1f05663ac6d72681bae182acfa37bf' into neighbors
    53361a1a Merge commit '90ddb2510ab9dd8bb2191898ff917c0b6e3c8a87' into latest-os-hpxml
    eee0df66 Merge commit 'b4cf7fdeb1ca9602115b030c312b19d6990b563a' into hpxml_required_args
    c0e81e2d Merge commit '8d0a6c637926b12d1677fe3d219fb56e1829e08d' into hpxml_required_args
    a0c7d026 Merge commit '0ca19a1b4988efc1249a7a321a264f807546d119' into latest-os-hpxml
    e92e6a2a Merge branch 'unconditioned_basement_bugfix' of https://github.com/NREL/OpenStudio-HEScore into defaults
    d492512c Bugfix for unconditioned basements.
    
    git-subtree-dir: measures/HPXMLtoOpenStudio
    git-subtree-split: ed7554257dd3852a709e7fd2d6b0249bd738e072

--- tests/hpxml_translator_test.rb
+++ tests/hpxml_translator_test.rb
@@ -92,11 +92,9 @@ class HPXMLTranslatorTest < MiniTest::Test
                                                             "Expected FractionHeatLoadServed to sum to <= 1, but calculated sum is 1.1."],
                             'missing-elements.xml' => ["Expected [1] element(s) but found 0 element(s) for xpath: /HPXML/Building/BuildingDetails/BuildingSummary/BuildingConstruction/NumberofConditionedFloors",
                                                        "Expected [1] element(s) but found 0 element(s) for xpath: /HPXML/Building/BuildingDetails/BuildingSummary/BuildingConstruction/ConditionedFloorArea"],
-                            'missing-surfaces.xml' => ["Thermal zone 'garage' must have at least one floor surface.",
-                                                       "Thermal zone 'garage' must have at least one roof/ceiling surface.",
-                                                       "Thermal zone 'garage' must have at least one surface adjacent to outside/ground."],
+                            'missing-surfaces.xml' => ["Thermal zone 'garage' must have at least two floor/roof/ceiling surfaces."],
                             'net-area-negative-wall.xml' => ["Calculated a negative net surface area for Wall 'Wall'."],
-                            'net-area-negative-roof.xml' => ["Calculated a negative net surface area for Roof 'AtticRoof'."],
+                            'net-area-negative-roof.xml' => ["Calculated a negative net surface area for Roof 'Roof'."],
                             'refrigerator-location.xml' => ["Refrigerator location is 'garage' but building does not have this location specified."],
                             'refrigerator-location-other.xml' => ["Expected [1] element(s) but found 0 element(s) for xpath: /HPXML/Building/BuildingDetails/Appliances/Refrigerator[Location="],
                             'unattached-cfis.xml' => ["Attached HVAC distribution system 'foobar' not found for mechanical ventilation 'MechanicalVentilation'."],
@@ -598,26 +596,6 @@ class HPXMLTranslatorTest < MiniTest::Test
             flunk "Unexpected heating system type '#{htg_sys_type}'."
           end
           assert_in_epsilon(hpxml_value, sql_value, 0.01)
-
-          if htg_sys_type == 'Furnace'
-            # Also check supply fan of cooling system as needed
-            htg_dist = htg_sys.elements['DistributionSystem']
-            bldg_details.elements.each('Systems/HVAC/HVACPlant/CoolingSystem[FractionCoolLoadServed > 0]') do |clg_sys|
-              clg_dist = clg_sys.elements['DistributionSystem']
-              next if htg_dist.nil? or clg_dist.nil?
-              next if clg_dist.attributes['idref'] != htg_dist.attributes['idref']
-
-              clg_sys_type = XMLHelper.get_value(clg_sys, 'CoolingSystemType')
-              if clg_sys_type == 'central air conditioning'
-                query_w = "SELECT Value FROM TabularDataWithStrings WHERE ReportName='EquipmentSummary' AND ReportForString='Entire Facility' AND TableName='Fans' AND RowName LIKE '%#{Constants.ObjectNameCentralAirConditioner.upcase}%' AND ColumnName='Rated Electric Power' AND Units='W'"
-                sql_value_w = sqlFile.execAndReturnFirstDouble(query_w).get
-                sql_value = sql_value_w * sql_value_htg_airflow / sql_value_fan_airflow
-                assert_in_epsilon(hpxml_value, sql_value, 0.01)
-              else
-                flunk "Unexpected cooling system type: #{clg_sys_type}."
-              end
-            end
-          end
         end
 
       end
