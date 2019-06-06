c1319d7b6e9ba9c0eaed588deb18232b674d82a5
Scott Horowitz <scott.horowitz@nrel.gov>
2019-06-06 16:50:26 -0600

    Bump version.

--- measure.rb
+++ measure.rb
@@ -94,7 +94,7 @@ class HPXMLTranslator < OpenStudio::Measure::ModelMeasure
     end
 
     # Check for correct versions of OS
-    os_version = "2.8.0"
+    os_version = "2.8.1"
     if OpenStudio.openStudioVersion != os_version
       fail "OpenStudio version #{os_version} is required."
     end
