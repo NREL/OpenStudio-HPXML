520dc1aba329504eba1ee4593b55df38cf73d044
Yueyue Zhou <Yueyue.Zhou@nrel.gov>
2019-12-05 10:23:09 -0700

    Added standby loss indirect sample file

--- resources/EPvalidator.rb
+++ resources/EPvalidator.rb
@@ -498,6 +498,7 @@ class EnergyPlusValidator
         "RelatedHVACSystem" => one, # HeatingSystem (boiler)
         "TankVolume" => one,
         "WaterHeaterInsulation/Jacket/JacketRValue" => zero_or_one, # Capable to model tank wrap insulation
+        "extension/StandbyLoss" => zero_or_one,
       },
 
       ## [WHType=CombiTankless]
