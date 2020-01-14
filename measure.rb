5d7a916e3849a614c5e5c9b4c4edeaa6ff406f71
Scott Horowitz <scott.horowitz@nrel.gov>
2020-01-14 10:44:25 -0700

    Allow ceiling fans to be modeled without also requiring lighting.

--- measure.rb
+++ measure.rb
@@ -2915,6 +2915,7 @@ class OSModel
     return if lighting.nil?
 
     lighting_values = HPXML.get_lighting_values(lighting: lighting)
+    return if lighting_values[:fraction_tier_i_interior].nil? # Either all or none of the values are nil
 
     if lighting_values[:fraction_tier_i_interior] + lighting_values[:fraction_tier_ii_interior] > 1
       fail "Fraction of qualifying interior lighting fixtures #{lighting_values[:fraction_tier_i_interior] + lighting_values[:fraction_tier_ii_interior]} is greater than 1."
