0b57f7f0fa4c9917dd0ad0d686f376f89318f65e
Ben Park <benjamin.park@nrel.gov>
2020-08-28 12:06:07 -0600

    Update measure.xml and run Rubocop.

--- HPXMLtoOpenStudio/resources/EPvalidator.rb
+++ HPXMLtoOpenStudio/resources/EPvalidator.rb
@@ -24,15 +24,15 @@ class EnergyPlusValidator
           assert_test = XMLHelper.get_attribute_value(assert_element, 'test').gsub('h:', '')
 
           hpxml_doc.xpath(parent_xpath).each do |context_element|
-            if not context_element.xpath(assert_test) # check if assert_test is false
-              assert_value = assert_element.children.text # the value of sch:assert
-              if rule_context == '/*' # Root
-                error_message = assert_value
-              else
-                error_message = assert_value.gsub(': ', [': ', parent_xpath, ': '].join('')) # insert parent xpath into the error message
-              end
-              errors << error_message
+            next unless not context_element.xpath(assert_test) # check if assert_test is false
+
+            assert_value = assert_element.children.text # the value of sch:assert
+            if rule_context == '/*' # Root
+              error_message = assert_value
+            else
+              error_message = assert_value.gsub(': ', [': ', parent_xpath, ': '].join('')) # insert parent xpath into the error message
             end
+            errors << error_message
           end
         end
       end
