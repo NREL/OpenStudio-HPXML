b476db65dca28bf7f6a82ea807688aced04cc4b9
Scott Horowitz <scott.horowitz@nrel.gov>
2020-09-02 11:16:03 -0600

    Simplify EPvalidator.rb a bit (e.g., remove extra nested loop, add error-handling around xpath execution).

--- HPXMLtoOpenStudio/resources/EPvalidator.rb
+++ HPXMLtoOpenStudio/resources/EPvalidator.rb
@@ -2,38 +2,44 @@
 
 class EnergyPlusValidator
   def self.run_validator(hpxml_doc)
-    # root path
-    root_path = File.absolute_path(File.join(File.dirname(__FILE__), '..', '..'))
     # load the Schematron xml
-    stron_path = File.join(root_path, 'HPXMLtoOpenStudio', 'resources', 'EPvalidator.xml')
+    stron_path = File.join(File.dirname(__FILE__), 'EPvalidator.xml')
 
     errors = []
     doc = XMLHelper.parse_file(stron_path)
-    XMLHelper.get_elements(doc, '/sch:schema/sch:pattern').each do |pattern|
-      XMLHelper.get_elements(pattern, 'sch:rule').each do |rule|
-        rule_context = XMLHelper.get_attribute_value(rule, 'context')
-        if rule_context == '/*' # Root
-          parent_xpath = '/HPXML'
-        else
-          parent_xpath = rule_context.gsub('h:', '')
-        end
+    XMLHelper.get_elements(doc, '/sch:schema/sch:pattern/sch:rule').each do |rule|
+      rule_context = XMLHelper.get_attribute_value(rule, 'context')
+      if rule_context == '/*' # Root
+        parent_xpath = '/HPXML'
+      else
+        parent_xpath = rule_context.gsub('h:', '')
+      end
 
-        next if hpxml_doc.xpath(parent_xpath).empty? # Skip if parent element doesn't exist
+      begin
+        context_elements = hpxml_doc.xpath(parent_xpath)
+      rescue
+        fail "Invalid xpath: #{parent_xpath}"
+      end
+      next if context_elements.empty? # Skip if context element doesn't exist
 
-        XMLHelper.get_elements(rule, 'sch:assert').each do |assert_element|
-          assert_test = XMLHelper.get_attribute_value(assert_element, 'test').gsub('h:', '')
+      XMLHelper.get_elements(rule, 'sch:assert').each do |assert_element|
+        assert_test = XMLHelper.get_attribute_value(assert_element, 'test').gsub('h:', '')
 
-          hpxml_doc.xpath(parent_xpath).each do |context_element|
-            next if context_element.xpath(assert_test) # check if assert_test is false
-
-            assert_value = assert_element.children.text # the value of sch:assert
-            if rule_context == '/*' # Root
-              error_message = assert_value
-            else
-              error_message = assert_value.gsub(': ', [': ', parent_xpath, ': '].join('')) # insert parent xpath into the error message
-            end
-            errors << error_message
+        context_elements.each do |context_element|
+          begin
+            xpath_result = context_element.xpath(assert_test)
+          rescue
+            fail "Invalid xpath: #{assert_test}"
           end
+          next if xpath_result # check if assert_test is false
+
+          assert_value = assert_element.children.text # the value of sch:assert
+          if rule_context == '/*' # Root
+            error_message = assert_value
+          else
+            error_message = assert_value.gsub(': ', ": #{parent_xpath}: ") # insert parent xpath into the error message
+          end
+          errors << error_message
         end
       end
     end
