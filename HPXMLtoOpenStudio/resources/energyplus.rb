5b02b7e6a3f676c9dba82a399faaa6a5de3464e0
jmaguire1 <Jeff.Maguire@nrel.gov>
2020-10-12 12:14:57 -0600

    Whoops

--- HPXMLtoOpenStudio/resources/energyplus.rb
+++ HPXMLtoOpenStudio/resources/energyplus.rb
@@ -10,6 +10,36 @@ class EPlus
   FuelTypeWoodPellets = 'OtherFuel2'
   FuelTypeCoal = 'Coal'
 
+  def self.fuel_type(hpxml_fuel)
+    # Name of fuel used as inputs to E+ objects
+    if [HPXML::FuelTypeElectricity].include? hpxml_fuel
+      return FuelTypeElectricity
+    elsif [HPXML::FuelTypeNaturalGas].include? hpxml_fuel
+      return FuelTypeNaturalGas
+    elsif [HPXML::FuelTypeOil,
+           HPXML::FuelTypeOil1,
+           HPXML::FuelTypeOil2,
+           HPXML::FuelTypeOil4,
+           HPXML::FuelTypeOil5or6,
+           HPXML::FuelTypeDiesel,
+           HPXML::FuelTypeKerosene].include? hpxml_fuel
+      return FuelTypeOil
+    elsif [HPXML::FuelTypePropane].include? hpxml_fuel
+      return FuelTypePropane
+    elsif [HPXML::FuelTypeWoodCord].include? hpxml_fuel
+      return FuelTypeWoodCord
+    elsif [HPXML::FuelTypeWoodPellets].include? hpxml_fuel
+      return FuelTypeWoodPellets
+    elsif [HPXML::FuelTypeCoal,
+           HPXML::FuelTypeCoalAnthracite,
+           HPXML::FuelTypeCoalBituminous,
+           HPXML::FuelTypeCoke].include? hpxml_fuel
+      return FuelTypeCoal
+    else
+      fail "Unexpected HPXML fuel '#{hpxml_fuel}'."
+    end
+  end
+
   def self.input_fuel_map(hpxml_fuel)
     # Name of fuel used as inputs to E+ objects
     if [HPXML::FuelTypeElectricity].include? hpxml_fuel
