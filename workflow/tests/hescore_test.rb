3821111df52f1e8bbe15b39d8a0aef5656645bb9
Scott Horowitz <scott.horowitz@nrel.gov>
2021-01-30 10:39:20 -0700

    Update workflow tests.

--- workflow/tests/hescore_test.rb
+++ workflow/tests/hescore_test.rb
@@ -124,6 +124,11 @@ class HEScoreTest < Minitest::Unit::TestCase
 
       # Check run.log for messages
       File.readlines(File.join(parent_dir, 'HEScoreDesign', 'run.log')).each do |log_line|
+        next if log_line.strip.empty?
+        next if log_line.start_with? 'Info: '
+        next if log_line.start_with? 'Executing command'
+        next if (log_line.start_with?('Heat ') || log_line.start_with?('Cool ')) && log_line.include?('=') # FIXME: Can remove this when https://github.com/NREL/OpenStudio-HPXML/pull/595 is pulled in
+        
         next if log_line.include? 'Warning: Could not load nokogiri, no HPXML validation performed.'
 
         # FIXME: Remove this warning when window/skylight U-factors are reasonable
