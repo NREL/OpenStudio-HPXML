0f1ce75184e91f7d053a63477f23dc6fc9b81950
Scott Horowitz <scott.horowitz@nrel.gov>
2021-02-06 11:58:41 -0700

    Remove accidental HeatingCapacity17F element in GSHP sample file.

--- tasks.rb
+++ tasks.rb
@@ -3331,7 +3331,9 @@ def set_hpxml_heat_pumps(hpxml_file, hpxml)
          'base-hvac-ground-to-air-heat-pump-cooling-only.xml',
          'base-hvac-mini-split-heat-pump-ducted-cooling-only.xml'].include? hpxml_file
     hpxml.heat_pumps[0].heating_capacity = 0
-    hpxml.heat_pumps[0].heating_capacity_17F = 0
+    if not ['base-hvac-ground-to-air-heat-pump-cooling-only.xml'].include? hpxml_file
+      hpxml.heat_pumps[0].heating_capacity_17F = 0
+    end
     hpxml.heat_pumps[0].fraction_heat_load_served = 0
     hpxml.heat_pumps[0].backup_heating_fuel = nil
     hpxml.heat_pumps[0].backup_heating_capacity = nil
