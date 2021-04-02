25f1ac590de8c05111699dd590f47061d181ce67
Joe Robertson <joseph.robertson@nrel.gov>
2021-04-02 08:58:27 -0700

    First cut at updated config.

--- workflow/tests/hpxml_translator_test.rb
+++ workflow/tests/hpxml_translator_test.rb
@@ -28,7 +28,7 @@ class HPXMLTest < MiniTest::Test
 
     xmls = []
     sample_files_dir = File.absolute_path(File.join(@this_dir, '..', 'sample_files'))
-    Dir["#{sample_files_dir}/*.xml"].sort.each do |xml|
+    Dir["#{sample_files_dir}/base.xml"].sort.each do |xml|
       xmls << File.absolute_path(xml)
     end
 
