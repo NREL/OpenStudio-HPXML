395b9a33704af0ff8110434bd36c1a7b1bbcb668
Joe Robertson <joseph.robertson@nrel.gov>
2021-04-21 07:14:29 -0700

    Clean up compare script.

--- workflow/tests/compare.rb
+++ workflow/tests/compare.rb
@@ -9,53 +9,46 @@ unless Dir.exist?(dir)
 end
 
 files.each do |file|
+  results = { 'base' => {}, 'feature' => {} }
+
   # load files
-  base_file = "base_results/#{file}"
-  base_rows = CSV.read(File.join(File.dirname(__FILE__), base_file))
-
-  begin
-    feature_file = "results/#{file}"
-    feature_rows = CSV.read(File.join(File.dirname(__FILE__), feature_file))
-  rescue
-    next
-    puts "Could not find #{feature_file}."
-  end
-
-  # get columns
-  base_cols = base_rows[0]
-  feature_cols = feature_rows[0]
-
-  # get data
-  base = {}
-  base_rows[1..-1].each do |row|
-    hpxml = row[0]
-    base[hpxml] = {}
-    row[1..-1].each_with_index do |field, i|
-      begin
-        base[hpxml][base_cols[i + 1]] = Float(field)
-      rescue
-      end
+  results.keys.each do |key|
+    begin
+      results[key]['file'] = "results/#{file}"
+      results[key]['rows'] = CSV.read(File.join(File.dirname(__FILE__), results[key]['file']))
+    rescue
+      puts "Could not find #{results[key]['file']}."
+      next
     end
   end
 
-  feature = {}
-  feature_rows[1..-1].each do |row|
-    hpxml = row[0]
-    feature[hpxml] = {}
-    row[1..-1].each_with_index do |field, i|
-      begin
-        feature[hpxml][feature_cols[i + 1]] = Float(field)
-      rescue
+  # get columns
+  results.keys.each do |key|
+    results[key]['cols'] = results[key]['rows'][0]
+  end
+
+  # get data
+  results.keys.each do |key|
+    results[key]['rows'][1..-1].each do |row|
+      hpxml = row[0]
+      results[key][hpxml] = {}
+      row[1..-1].each_with_index do |field, i|
+        begin
+          results[key][hpxml][results[key]['cols'][i + 1]] = Float(field)
+        rescue
+        end
       end
     end
   end
 
   # get hpxml union
-  base_hpxmls = base_rows.transpose[0][1..-1]
-  feature_hpxmls = feature_rows.transpose[0][1..-1]
+  base_hpxmls = results['base']['rows'].transpose[0][1..-1]
+  feature_hpxmls = results['feature']['rows'].transpose[0][1..-1]
   hpxmls = base_hpxmls | feature_hpxmls
 
   # get column union
+  base_cols = results['base']['cols']
+  feature_cols = results['feature']['cols']
   cols = base_cols | feature_cols
 
   # create comparison table
@@ -66,20 +59,11 @@ files.each do |file|
       next if i == 0
 
       begin
-        base_field = base[hpxml][col]
-      rescue
-        base_field = nil
-      end
-
-      begin
-        feature_field = feature[hpxml][col]
-      rescue
-        feature_field = nil
-      end
-
-      m = 'N/A'
-      if (not base_field.nil?) && (not feature_field.nil?)
+        base_field = results['base'][hpxml][col]
+        feature_field = results['feature'][hpxml][col]
         m = "#{(feature_field - base_field).round(1)}"
+      rescue
+        m = 'N/A'
       end
 
       row << m
