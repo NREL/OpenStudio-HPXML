5be6bc7c799ac78f97fde2031000061291eabd81
Scott Horowitz <scott.horowitz@nrel.gov>
2021-04-29 17:06:53 -0600

    Re-enable code coverage (posted as CI artifacts).

--- HPXMLtoOpenStudio/resources/minitest_helper.rb
+++ HPXMLtoOpenStudio/resources/minitest_helper.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
-require 'minitest/autorun'
+if ENV['CI']
+  require 'simplecov'
+  SimpleCov.coverage_dir(File.join(Dir.getwd,"coverage"))
+  SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
+  SimpleCov.start
+end
 
+require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/reporters/spec_reporter' # Needed when run via OS CLI
-
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new # spec-like progress
