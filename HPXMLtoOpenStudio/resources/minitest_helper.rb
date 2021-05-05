03188d6b53fe1c3e0438236f64c8eb83ef60e069
Scott Horowitz <scott.horowitz@nrel.gov>
2021-05-04 18:35:13 -0600

    Prevent warnings in a few other situations (tasks and unit tests).

--- HPXMLtoOpenStudio/resources/minitest_helper.rb
+++ HPXMLtoOpenStudio/resources/minitest_helper.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+$VERBOSE = nil # Prevents ruby warnings, see https://github.com/NREL/OpenStudio/issues/4301
+
 called_from_cli = true
 begin
   OpenStudio.getOpenStudioCLI
--- tasks.rb
+++ tasks.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+$VERBOSE = nil # Prevents ruby warnings, see https://github.com/NREL/OpenStudio/issues/4301
+
 def create_hpxmls
   require 'oga'
   require_relative 'HPXMLtoOpenStudio/resources/constants'
