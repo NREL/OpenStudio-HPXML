4439f11814c590c04d3d2ee12f8e976e82a7d607
Yueyue Zhou <yueyue.zhou@nrel.gov>
2021-05-04 19:40:58 -0600

    Revert a few changes

--- HPXMLtoOpenStudio/measure.rb
+++ HPXMLtoOpenStudio/measure.rb
@@ -1628,12 +1628,13 @@ class OSModel
       heating_system = hvac_system[:heating]
 
       check_distribution_system(cooling_system.distribution_system, cooling_system.cooling_system_type)
+      is_ddb_control = (not hvac_control.onoff_thermostat_deadband.nil?) && (hvac_control.onoff_thermostat_deadband > 0) && (cooling_system.additional_properties.num_speeds == 1)
 
       if [HPXML::HVACTypeCentralAirConditioner].include? cooling_system.cooling_system_type
 
         HVAC.apply_central_air_conditioner_furnace(model, runner, cooling_system, heating_system,
                                                    @remaining_cool_load_frac, @remaining_heat_load_frac,
-                                                   living_zone, @hvac_map, @hpxml.hvac_controls[0])
+                                                   living_zone, @hvac_map, is_ddb_control)
 
         if not heating_system.nil?
           @remaining_heat_load_frac -= heating_system.fraction_heat_load_served
@@ -1643,7 +1644,7 @@ class OSModel
 
         HVAC.apply_room_air_conditioner(model, runner, cooling_system,
                                         @remaining_cool_load_frac, living_zone,
-                                        @hvac_map, @hpxml.hvac_controls[0])
+                                        @hvac_map, is_ddb_control)
 
       elsif [HPXML::HVACTypeEvaporativeCooler].include? cooling_system.cooling_system_type
 
@@ -1719,6 +1720,7 @@ class OSModel
       heat_pump = hvac_system[:cooling]
 
       check_distribution_system(heat_pump.distribution_system, heat_pump.heat_pump_type)
+      is_ddb_control = (not hvac_control.onoff_thermostat_deadband.nil?) && (hvac_control.onoff_thermostat_deadband > 0) && (heat_pump.additional_properties.num_speeds == 1)
 
       if [HPXML::HVACTypeHeatPumpWaterLoopToAir].include? heat_pump.heat_pump_type
 
@@ -1732,7 +1734,7 @@ class OSModel
         HVAC.apply_central_air_to_air_heat_pump(model, runner, heat_pump,
                                                 @remaining_heat_load_frac,
                                                 @remaining_cool_load_frac,
-                                                living_zone, @hvac_map, @hpxml.hvac_controls[0])
+                                                living_zone, @hvac_map, is_ddb_control)
 
       elsif [HPXML::HVACTypeHeatPumpMiniSplit].include? heat_pump.heat_pump_type
 
--- tasks.rb
+++ tasks.rb
@@ -393,7 +393,6 @@ def create_hpxmls
     'base-hvac-portable-heater-gas-only.xml' => 'base.xml',
     'base-hvac-programmable-thermostat.xml' => 'base.xml',
     'base-hvac-onoff-thermostat-deadband.xml' => 'base-hvac-air-to-air-heat-pump-1-speed.xml',
-    'base-hvac-realistic-control-2-speed.xml' => 'base-hvac-air-to-air-heat-pump-2-speed.xml',
     'base-hvac-programmable-thermostat-detailed.xml' => 'base.xml',
     'base-hvac-room-ac-only.xml' => 'base.xml',
     'base-hvac-room-ac-only-33percent.xml' => 'base-hvac-room-ac-only.xml',
@@ -646,8 +645,6 @@ def set_hpxml_header(hpxml_file, hpxml)
   elsif ['base-misc-defaults.xml'].include? hpxml_file
     hpxml.header.timestep = nil
   elsif ['base-hvac-onoff-thermostat-deadband.xml'].include? hpxml_file
-    hpxml.header.timestep = 1
-  elsif ['base-hvac-realistic-control-2-speed.xml'].include? hpxml_file
     hpxml.header.timestep = 1
   elsif ['invalid_files/invalid-input-parameters.xml'].include? hpxml_file
     hpxml.header.transaction = 'modify'
@@ -3539,9 +3536,6 @@ def set_hpxml_hvac_control(hpxml_file, hpxml)
     hpxml.hvac_controls[0].ceiling_fan_cooling_setpoint_temp_offset = 0.5
   elsif ['base-hvac-onoff-thermostat-deadband.xml'].include? hpxml_file
     hpxml.hvac_controls[0].onoff_thermostat_deadband = 2.0
-  elsif ['base-hvac-realistic-control-2-speed.xml'].include? hpxml_file
-    hpxml.hvac_controls[0].onoff_thermostat_deadband = 2.0
-    hpxml.hvac_controls[0].realistic_staging = true
   end
 end
 
