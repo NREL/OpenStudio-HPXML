fa8c8bfefa1f3307161f81335b7431689a712adb
Joe Robertson <joseph.robertson@nrel.gov>
2021-06-30 13:41:12 -0700

    Updates for groupby.

--- workflow/tests/compare.py
+++ workflow/tests/compare.py
@@ -8,7 +8,7 @@ from plotly.subplots import make_subplots
 
 class Compare:
 
-  def samples(self, base_folder, feature_folder, export_folder):
+  def samples(self, base_folder, feature_folder, export_folder, groupby=[]):
 
     def value_counts(df, file):
       value_counts = []
@@ -39,28 +39,38 @@ class Compare:
     value_counts(df, file)
 
 
-  def results(self, base_folder, feature_folder, export_folder, aggregate_by=[]):
+  def results(self, base_folder, feature_folder, export_folder, groupby=[]):
     files = []
     for file in os.listdir(base_folder):
-      if file.endswith('.csv'):
+      if file.startswith('results') and file.endswith('.csv'):
         files.append(file)
 
     for file in files:
       base_df = pd.read_csv(os.path.join(base_folder, file), index_col=0)
       feature_df = pd.read_csv(os.path.join(feature_folder, file), index_col=0)
 
-      df = feature_df - base_df
+      try:
+        df = feature_df - base_df
+      except:
+        df = feature_df != base_df
+        df = df.astype(int)
+
       df = df.fillna('NA')
       df.to_csv(os.path.join(export_folder, file))
 
-  def visualize(self, base_folder, feature_folder, export_folder):
-    
+  def visualize(self, base_folder, feature_folder, export_folder, groupby=[]):
+    excludes = ['buildstock.csv', 'results_characteristics.csv']
+
+    if groupby:
+      base_characteristics_df = pd.read_csv(os.path.join(base_folder, 'results_characteristics.csv'), index_col=0)[groupby]
+      feature_characteristics_df = pd.read_csv(os.path.join(feature_folder, 'results_characteristics.csv'), index_col=0)[groupby]
+
     def get_min_max(x_col, y_col, min_value, max_value):
         if 0.9 * np.min([x_col.min(), y_col.min()]) < min_value:
                         min_value = 0.9 * np.min([x_col.min(), y_col.min()])
         if 1.1 * np.max([x_col.max(), y_col.max()]) > max_value:
                         max_value = 1.1 * np.max([x_col.max(), y_col.max()])
-        
+
         return(min_value, max_value)
 
     def add_error_lines(fig, showlegend, row, col, min_value, max_value):
@@ -70,7 +80,7 @@ class Compare:
 
     files = []
     for file in os.listdir(base_folder):
-      if file.endswith('.csv'):
+      if not file in excludes:
         files.append(file)
 
     for file in files:
@@ -78,33 +88,48 @@ class Compare:
       feature_df = pd.read_csv(os.path.join(feature_folder, file), index_col=0)
 
       end_uses = sorted(list(set(base_df.columns) | set(feature_df.columns)))
-      if file == 'results.csv':
-        end_uses = [x for x in end_uses if 'Fuel Use' in x]
+      if file == 'results.csv' or file == 'results_output.csv':
+        end_uses = [x for x in end_uses if 'Fuel Use:' in x or 'fuel_use_' in x] # FIXME
 
-      fig = make_subplots(rows=len(end_uses), cols=1, row_titles=[f'<b>{f}</b>' for f in end_uses], vertical_spacing = 0.015)
+      if groupby:
+        base_df = base_characteristics_df.join(base_df)
+        feature_df = feature_characteristics_df.join(feature_df)
+        groups = list(base_df[groupby[0]].unique())
+      else:
+        groups = ['1:1']
+
+      fig = make_subplots(rows=len(end_uses), cols=len(groups), subplot_titles=groups*len(end_uses), row_titles=[f'<b>{f}</b>' for f in end_uses], vertical_spacing = 0.015)
 
       row = 0
       for end_use in end_uses:
         row += 1
-        col = 1
-        showlegend = False
-        if row == 1: showlegend = True
+        for group in groups:
+          col = groups.index(group) + 1
+          showlegend = False
+          if col == 1 and row == 1: showlegend = True
 
-        x = base_df
-        y = feature_df
+          x = base_df
+          y = feature_df
 
-        fig.add_trace(go.Scatter(x=x[end_use], y=y[end_use], marker=dict(size=8), mode='markers', name=end_use, legendgroup=end_use, showlegend=False), row=row, col=col)
+          if groupby:
+            x = x.loc[x[groupby[0]] == group, :]
+            y = y.loc[y[groupby[0]] == group, :]
 
-        min_value, max_value = get_min_max(x[end_use], y[end_use], 0, 0)
-        add_error_lines(fig, showlegend, row, col, min_value, max_value)
-        fig.update_xaxes(title_text='base', row=row, col=col)
-        fig.update_yaxes(title_text='feature', row=row, col=col)
+          fig.add_trace(go.Scatter(x=x[end_use], y=y[end_use], marker=dict(size=8), mode='markers', name=end_use, legendgroup=end_use, showlegend=False), row=row, col=col)
+
+          min_value, max_value = get_min_max(x[end_use], y[end_use], 0, 0)
+          add_error_lines(fig, showlegend, row, col, min_value, max_value)
+          fig.update_xaxes(title_text='base', row=row, col=col)
+          fig.update_yaxes(title_text='feature', row=row, col=col)
 
       fig['layout'].update(title=file, template='plotly_white')
-      fig.update_layout(width=800, height=600*len(end_uses), autosize=False, font=dict(size=12))
+      fig.update_layout(width=800*len(groups), height=600*len(end_uses), autosize=False, font=dict(size=12))
       for i in fig['layout']['annotations']:
           i['font'] = dict(size=12) if i['text'] in end_uses else dict(size=12)
-      fig.write_image(os.path.join(export_folder, file.replace('csv', 'svg')))
+      basename, ext = os.path.splitext(file)
+      if groupby:
+        basename += '_{groupby}'.format(groupby=groupby[0])
+      fig.write_image(os.path.join(export_folder, '{basename}.svg'.format(basename=basename)))
 
 if __name__ == '__main__':
 
@@ -114,11 +139,15 @@ if __name__ == '__main__':
 
   actions = [method for method in dir(Compare) if method.startswith('__') is False]
 
+  groupby = ['build_existing_model.geometry_building_type_recs']
+
   parser = argparse.ArgumentParser()
   parser.add_argument('-b', '--base_folder', default=default_base_folder, help='TODO')
   parser.add_argument('-f', '--feature_folder', default=default_feature_folder, help='TODO')
   parser.add_argument('-a', '--actions', action='append', choices=actions, help='TODO')
   parser.add_argument('-e', '--export_folder', default=default_export_folder, help='TODO')
+  parser.add_argument('-g', '--groupby', action='append', choices=groupby, help='TODO')
+  parser.add_argument('-m', '--mapping', help='TODO')
   args = parser.parse_args()
 
   if not os.path.exists(args.export_folder):
@@ -129,5 +158,8 @@ if __name__ == '__main__':
   if args.actions == None:
     args.actions = []
 
+  if args.groupby == None:
+    args.groupby = []
+
   for action in args.actions:
-    getattr(compare, action)(args.base_folder, args.feature_folder, args.export_folder)
+    getattr(compare, action)(args.base_folder, args.feature_folder, args.export_folder, args.groupby)
