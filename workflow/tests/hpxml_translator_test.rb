e1e43d5718bcdf1876e314b96a73d613e2c0bbd8
Scott Horowitz <scott.horowitz@nrel.gov>
2021-10-25 14:17:31 -0600

    Minor cleanup.

--- workflow/tests/hpxml_translator_test.rb
+++ workflow/tests/hpxml_translator_test.rb
@@ -598,8 +598,7 @@ class HPXMLTest < MiniTest::Test
       next if err_line.include? 'sensible part-load ratio out of range error continues'
       next if err_line.include? 'Iteration limit exceeded in calculating sensible part-load ratio error continues'
       next if err_line.include?('setupIHGOutputs: Output variables=Zone Other Equipment') && err_line.include?('are not available.')
-      next if err_line.include?('setupIHGOutputs: Output variables=Space Other Equipment') && err_line.include?('are not available') # FIXME: Should be able to remove this with updated E+ rc release
-      next if err_line.include? 'Actual air mass flow rate is smaller than 25% of water-to-air heat pump coil rated air flow rate.' # FIXME: Investigate this
+      next if err_line.include? 'Actual air mass flow rate is smaller than 25% of water-to-air heat pump coil rated air flow rate.' # FUTURE: Remove this when https://github.com/NREL/EnergyPlus/issues/9125 is resolved
 
       # HPWHs
       if hpxml.water_heating_systems.select { |wh| wh.water_heater_type == HPXML::WaterHeaterTypeHeatPump }.size > 0
