ca2d47d8f603289168925b07d9e2a312bbc00bea
Joe Robertson <joseph.robertson@nrel.gov>
2022-01-13 12:50:24 -0700

    Add util resource to bills measure.

---
+++ ReportUtilityBills/resources/util.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class UtilityBill
+  def self.calculate_simple(fuels, fuel_type, utility_bill, fixed_charge, marginal_rate)
+    energy = fuels[fuel_type].timeseries_output
+
+    utility_bill.fixed = 12.0 * fixed_charge unless energy.all? { |x| x == 0.0 }
+    utility_bill.marginal = energy.collect { |x| x * marginal_rate }.sum
+    utility_bill.total = utility_bill.fixed + utility_bill.marginal
+  end
+end
