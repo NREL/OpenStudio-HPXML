614ba40777fc6c632974f4dacfd0b7f6354c919c
Joe Robertson <joseph.robertson@nrel.gov>
2022-01-14 16:00:50 -0700

    Simplify code a bit.

--- ReportUtilityBills/resources/util.rb
+++ ReportUtilityBills/resources/util.rb
@@ -4,11 +4,8 @@ class UtilityBill
   def self.calculate_simple(fuels, fuel_type, utility_bill, fixed_charge, marginal_rate)
     energy = fuels[fuel_type].timeseries_output
 
-    if [FT::Elec, FT::Gas].include? fuel_type
-      utility_bill.fixed = 12.0 * fixed_charge if !energy.all? { |x| x == 0.0 }
-    end
+    utility_bill.fixed = 12.0 * fixed_charge if (!energy.all? { |x| x == 0.0 } && !fixed_charge.nil?)
     utility_bill.marginal = energy.collect { |x| x * marginal_rate }.sum
-    utility_bill.total = utility_bill.marginal
-    utility_bill.total += utility_bill.fixed if !utility_bill.fixed.nil?
+    utility_bill.total = utility_bill.fixed + utility_bill.marginal
   end
 end
