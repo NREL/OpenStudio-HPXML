63f9383b56ea27a326962edeef202a37e9f380be
jmaguire1 <Jeff.Maguire@nrel.gov>
2022-04-29 10:20:05 -0600

    Allow a warning about tank setpoint temperature higher than HP cut-in temp. Occurs if HPWH is installed outside and EMS program forces the HP off

--- workflow/tests/hpxml_translator_test.rb
+++ workflow/tests/hpxml_translator_test.rb
@@ -718,6 +718,7 @@ class HPXMLTest < MiniTest::Test
         next if err_line.include? 'Rated air volume flow rate per watt of rated total water heating capacity is out of range'
         next if err_line.include? 'For object = Coil:WaterHeating:AirToWaterHeatPump:Wrapped'
         next if err_line.include? 'Enthalpy out of range (PsyTsatFnHPb)'
+        next if err_line.include? 'Water heater tank set point temperature is greater than or equal to the cut-in temperature of the heat pump water heater.'
       end
       # HP defrost curves
       if hpxml.heat_pumps.select { |hp| [HPXML::HVACTypeHeatPumpAirToAir, HPXML::HVACTypeHeatPumpMiniSplit, HPXML::HVACTypeHeatPumpPTHP].include? hp.heat_pump_type }.size > 0
@@ -751,9 +752,6 @@ class HPXMLTest < MiniTest::Test
       # FIXME: are these warnings expected?
       if hpxml.water_heating_systems.select { |wh| wh.tank_model_type == HPXML::WaterHeaterTankModelTypeStratified }.size > 0
         next if err_line.include? 'Recovery Efficiency and Energy Factor could not be calculated during the test for standard ratings'
-      end
-      if hpxml.water_heating_systems.select { |wh| wh.water_heater_type == HPXML::WaterHeaterTypeHeatPump && !hpxml.header.schedules_filepaths.empty? }.size > 0
-        next if err_line.include? 'Water heater tank set point temperature is greater than or equal to the cut-in temperature of the heat pump water heater.'
       end
 
       flunk "Unexpected warning found: #{err_line}"
