090e683863c360156c3123ca91fc51e9c7bd3127
Joe Robertson <joseph.robertson@nrel.gov>
2022-08-25 14:38:55 -0700

    Shift for dst only if enabled.

--- ReportSimulationOutput/measure.rb
+++ ReportSimulationOutput/measure.rb
@@ -1774,14 +1774,16 @@ class ReportSimulationOutput < OpenStudio::Measure::ReportingMeasure
 
         # Apply daylight savings
         # https://github.com/NREL/BEopt2/blob/master/Modeling/output.py#L580-L583
-        dst_start_ix = nil
-        dst_end_ix = nil
-        @timestamps.zip(timestamps_dst).each_with_index do |ts, i|
-          dst_start_ix = i if ts[0] != ts[1] && dst_start_ix.nil?
-          dst_end_ix = i if ts[0] == ts[1] && dst_end_ix.nil? && !dst_start_ix.nil?
-        end
-        dst_end_ix.downto(dst_start_ix + 1) do |i|
-          data[i + 1] = data[i]
+        if @hpxml.header.dst_enabled
+          dst_start_ix = nil
+          dst_end_ix = nil
+          @timestamps.zip(timestamps_dst).each_with_index do |ts, i|
+            dst_start_ix = i if ts[0] != ts[1] && dst_start_ix.nil?
+            dst_end_ix = i if ts[0] == ts[1] && dst_end_ix.nil? && !dst_start_ix.nil?
+          end
+          dst_end_ix.downto(dst_start_ix + 1) do |i|
+            data[i + 1] = data[i]
+          end
         end
 
         data.insert(0, *header_data) # Add header data to beginning
