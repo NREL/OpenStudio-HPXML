ebaef19155422daf7aa95f33f6ec43286771b851
Joe Robertson <joseph.robertson@nrel.gov>
2022-10-10 11:20:50 -0700

    Add new sample file and update docs.

--- tasks.rb
+++ tasks.rb
@@ -376,6 +376,7 @@ def create_hpxmls
     'base-mechvent-erv.xml' => 'base.xml',
     'base-mechvent-erv-atre-asre.xml' => 'base.xml',
     'base-mechvent-exhaust.xml' => 'base.xml',
+    'base-mechvent-exhaust-detailed-availability.xml' => 'base-mechvent-exhaust.xml',
     'base-mechvent-exhaust-rated-flow-rate.xml' => 'base.xml',
     'base-mechvent-hrv.xml' => 'base.xml',
     'base-mechvent-hrv-asre.xml' => 'base.xml',
@@ -2523,6 +2524,11 @@ def set_measure_argument_values(hpxml_file, args, sch_args, orig_parent)
     args.delete('bathroom_fans_hours_in_operation')
     args.delete('bathroom_fans_start_hour')
     args['schedules_filepaths'] = '../../HPXMLtoOpenStudio/resources/schedule_files/local-ventilation.csv'
+  end
+
+  # House Ventilation Schedules
+  if ['base-mechvent-exhaust-detailed-availability.xml'].include? hpxml_file
+    args['schedules_filepaths'] = '../../HPXMLtoOpenStudio/resources/schedule_files/house-fan.csv'
   end
 
   # Water Heater Schedules
