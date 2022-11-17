fe2a752a4b91015deb33a291a1cff72c792effd2
Joe Robertson <joseph.robertson@nrel.gov>
2022-11-17 13:42:49 -0700

    Disable more lifetime model stuff.

--- HPXMLtoOpenStudio/resources/battery.rb
+++ HPXMLtoOpenStudio/resources/battery.rb
@@ -68,7 +68,7 @@ class Battery
       elcs.setThermalZone(battery.additional_properties.space.thermalZone.get)
     end
     elcs.setRadiativeFraction(0.9 * frac_sens)
-    elcs.setLifetimeModel(battery.lifetime_model)
+    # elcs.setLifetimeModel(battery.lifetime_model)
     elcs.setNumberofCellsinSeries(number_of_cells_in_series)
     elcs.setNumberofStringsinParallel(number_of_strings_in_parallel)
     elcs.setInitialFractionalStateofCharge(0.0)
