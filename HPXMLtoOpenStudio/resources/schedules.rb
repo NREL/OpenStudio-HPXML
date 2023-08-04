8ceda701c43a0b1f26eea60fea59b52d6e3fa3c5
Joe Robertson <joseph.robertson@nrel.gov>
2023-08-04 15:34:40 -0700

    Set unit_model on schedules_file instance.

--- HPXMLtoOpenStudio/resources/schedules.rb
+++ HPXMLtoOpenStudio/resources/schedules.rb
@@ -1397,6 +1397,10 @@ class SchedulesFile
     export()
   end
 
+  def set_unit_model(unit_model)
+    @model = unit_model
+  end
+
   def nil?
     if @schedules.nil?
       return true
--- workflow/tests/hpxml_translator_test.rb
+++ workflow/tests/hpxml_translator_test.rb
@@ -30,6 +30,7 @@ class HPXMLTest < Minitest::Test
     sample_files_dirs.each do |sample_files_dir|
       Dir["#{sample_files_dir}/*.xml"].sort.each do |xml|
         next if xml.include? 'base-multiple-buildings.xml' # This is tested in test_multiple_building_ids
+        next if xml.include? 'base-two-buildings'
         # FIXME: Delete this line when E+ fix is available; currently hitting limitation noted
         # in https://github.com/NREL/EnergyPlus/issues/9049
         next if xml.include? 'base-bldgtype-multifamily-shared-mechvent-multiple.xml'
