ee18aabdc5f16e7ec75c2c7a1bcf41269ff81cff
Scott Horowitz <scott.horowitz@nrel.gov>
2023-09-16 13:39:19 -0600

    A little more debug

--- HPXMLtoOpenStudio/resources/xmlvalidator.rb
+++ HPXMLtoOpenStudio/resources/xmlvalidator.rb
@@ -56,7 +56,8 @@ class XMLValidator
           # Try to retrieve SystemIdentifier
           context_element = hpxml_doc.xpath(current_context.gsub('h:', ''))[current_context_idx]
           if context_element.nil?
-            puts report_doc.to_xml # FIXME: Temporary
+            puts "hpxml_path: #{hpxml_path}" # FIXME: Temporary
+            puts "report_doc: #{report_doc.to_xml}" # FIXME: Temporary
             fail "Could not find element at xpath '#{current_context}' with index #{current_context_idx}."
           end
 
