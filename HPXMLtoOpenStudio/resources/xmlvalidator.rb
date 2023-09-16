77bdf2d4c9ac8bc8468e233982ec27dd6ea96901
Scott Horowitz <scott.horowitz@nrel.gov>
2023-09-16 13:38:34 -0600

    Debug

--- HPXMLtoOpenStudio/resources/xmlvalidator.rb
+++ HPXMLtoOpenStudio/resources/xmlvalidator.rb
@@ -56,6 +56,7 @@ class XMLValidator
           # Try to retrieve SystemIdentifier
           context_element = hpxml_doc.xpath(current_context.gsub('h:', ''))[current_context_idx]
           if context_element.nil?
+            puts report_doc.to_xml # FIXME: Temporary
             fail "Could not find element at xpath '#{current_context}' with index #{current_context_idx}."
           end
 
