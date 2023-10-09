d7a214ed92fc28901913be9aa7ac4261b4a30bfb
Joe Robertson <joseph.robertson@nrel.gov>
2023-10-09 14:31:40 -0700

    Remove random seed arg from tasks.

--- tasks.rb
+++ tasks.rb
@@ -66,7 +66,6 @@ def create_hpxmls
       csv_path = json_input['schedules_filepaths'].to_s.split(',').map(&:strip).find { |fp| fp.include? 'occupancy-stochastic' }
       if (not csv_path.nil?) && (not schedules_regenerated.include? csv_path)
         sch_args = { 'hpxml_path' => hpxml_path,
-                     'schedules_random_seed' => i,
                      'output_csv_path' => csv_path,
                      'hpxml_output_path' => hpxml_path,
                      'building_id' => "MyBuilding#{suffix}" }
