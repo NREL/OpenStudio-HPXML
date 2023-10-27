b7c93e44f4513bee345f75b6f75102eb7f7d5389
Joe Robertson <joseph.robertson@nrel.gov>
2023-10-27 09:11:07 -0700

    Confirm new schedulefile setter does what we want.

--- HPXMLtoOpenStudio/resources/schedules.rb
+++ HPXMLtoOpenStudio/resources/schedules.rb
@@ -1526,6 +1526,7 @@ class SchedulesFile
     schedule_file.setRowstoSkipatTop(rows_to_skip)
     schedule_file.setNumberofHoursofData(num_hrs_in_year.to_i)
     schedule_file.setMinutesperItem(min_per_item.to_i)
+    schedule_file.setTranslateFileName(true)
 
     Schedule.set_schedule_type_limits(@model, schedule_file, schedule_type_limits_name)
 
