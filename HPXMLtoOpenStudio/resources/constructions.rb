95bc1701a749a8be472895d4eea4e4ee9d492b11
Joe Robertson <joseph.robertson@nrel.gov>
2023-12-06 10:18:48 -0700

    Update radiant barrier arg names in build test file.

--- HPXMLtoOpenStudio/resources/constructions.rb
+++ HPXMLtoOpenStudio/resources/constructions.rb
@@ -1888,19 +1888,19 @@ class Constructions
   end
 
   def self.apply_wall_construction(runner,
-                                    model,
-                                    surfaces,
-                                    wall_id,
-                                    wall_type,
-                                    assembly_r,
-                                    mat_int_finish,
-                                    has_radiant_barrier,
-                                    inside_film,
-                                    outside_film,
-                                    radiant_barrier_grade,
-                                    mat_ext_finish,
-                                    solar_absorptance,
-                                    emittance)
+                                   model,
+                                   surfaces,
+                                   wall_id,
+                                   wall_type,
+                                   assembly_r,
+                                   mat_int_finish,
+                                   has_radiant_barrier,
+                                   inside_film,
+                                   outside_film,
+                                   radiant_barrier_grade,
+                                   mat_ext_finish,
+                                   solar_absorptance,
+                                   emittance)
 
     if mat_ext_finish.nil?
       fallback_mat_ext_finish = nil
