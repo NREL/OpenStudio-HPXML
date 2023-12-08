bee16722c222af2aa7f984b24eff135c47dce659
Scott Horowitz <scott.horowitz@nrel.gov>
2023-12-08 14:41:12 -0700

    Improve handling for when bore depth or # bores (but not both) are defaulted. Simplify docs related to soil inputs.

--- HPXMLtoOpenStudio/tests/test_hvac_sizing.rb
+++ HPXMLtoOpenStudio/tests/test_hvac_sizing.rb
@@ -477,7 +477,7 @@ class HPXMLtoOpenStudioHVACSizingTest < Minitest::Test
     XMLHelper.write_file(hpxml.to_doc, @tmp_hpxml_path)
     _model, _test_hpxml, test_hpxml_bldg = _test_measure(args_hash)
     assert_equal(3, test_hpxml_bldg.geothermal_loops[0].num_bore_holes)
-    assert_in_epsilon(688.5 / 3 + 5.0, test_hpxml_bldg.geothermal_loops[0].bore_length, 0.01)
+    assert_in_epsilon(688.5 / 3, test_hpxml_bldg.geothermal_loops[0].bore_length, 0.01)
 
     # Bore depth greater than the max -> increase number of boreholes
     hpxml, hpxml_bldg = _create_hpxml('base-hvac-ground-to-air-heat-pump.xml')
@@ -485,7 +485,7 @@ class HPXMLtoOpenStudioHVACSizingTest < Minitest::Test
     XMLHelper.write_file(hpxml.to_doc, @tmp_hpxml_path)
     _model, _test_hpxml, test_hpxml_bldg = _test_measure(args_hash)
     assert_equal(5, test_hpxml_bldg.geothermal_loops[0].num_bore_holes)
-    assert_in_epsilon(2062.9 / 5 + 5, test_hpxml_bldg.geothermal_loops[0].bore_length, 0.01)
+    assert_in_epsilon(2062.9 / 5, test_hpxml_bldg.geothermal_loops[0].bore_length, 0.01)
 
     # Bore depth greater than the max -> increase number of boreholes until the max, set depth to the max, and issue warning
     hpxml, hpxml_bldg = _create_hpxml('base-hvac-ground-to-air-heat-pump.xml')
@@ -501,7 +501,7 @@ class HPXMLtoOpenStudioHVACSizingTest < Minitest::Test
     XMLHelper.write_file(hpxml.to_doc, @tmp_hpxml_path)
     _model, _test_hpxml, test_hpxml_bldg = _test_measure(args_hash)
     assert_equal(10, test_hpxml_bldg.geothermal_loops[0].num_bore_holes)
-    assert_in_epsilon(3187.0 / 10 + 5, test_hpxml_bldg.geothermal_loops[0].bore_length, 0.01)
+    assert_in_epsilon(3187.0 / 10, test_hpxml_bldg.geothermal_loops[0].bore_length, 0.01)
   end
 
   def test_g_function_library_linear_interpolation_example
