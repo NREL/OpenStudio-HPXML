323632e6b98e081e0b0a23de34b22e1f6315b7cf
Scott Horowitz <scott.horowitz@nrel.gov>
2024-01-05 12:48:44 -0700

    Switch to error log level.

--- HPXMLtoOpenStudio/resources/minitest_helper.rb
+++ HPXMLtoOpenStudio/resources/minitest_helper.rb
@@ -3,7 +3,7 @@
 called_from_cli = true
 begin
   OpenStudio.getOpenStudioCLI
-  OpenStudio::Logger.instance.standardOutLogger.setLogLevel(OpenStudio::Fatal)
+  OpenStudio::Logger.instance.standardOutLogger.setLogLevel(OpenStudio::Error)
 rescue
   called_from_cli = false
 end
