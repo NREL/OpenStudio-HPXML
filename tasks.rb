6dbbac11f5a13bed7a3bb6b814b40e5a168524a8
Joe Robertson <joseph.robertson@nrel.gov>
2024-01-23 10:30:34 -0700

    Fix new shared scheduled recirc sample file.

--- tasks.rb
+++ tasks.rb
@@ -1691,7 +1691,8 @@ def apply_hpxml_modification(hpxml_file, hpxml)
       hpxml_bldg.water_heating.water_fixtures_weekday_fractions = '0.012, 0.006, 0.004, 0.005, 0.010, 0.034, 0.078, 0.087, 0.080, 0.067, 0.056, 0.047, 0.040, 0.035, 0.033, 0.031, 0.039, 0.051, 0.060, 0.060, 0.055, 0.048, 0.038, 0.026'
       hpxml_bldg.water_heating.water_fixtures_weekend_fractions = '0.012, 0.006, 0.004, 0.005, 0.010, 0.034, 0.078, 0.087, 0.080, 0.067, 0.056, 0.047, 0.040, 0.035, 0.033, 0.031, 0.039, 0.051, 0.060, 0.060, 0.055, 0.048, 0.038, 0.026'
       hpxml_bldg.water_heating.water_fixtures_monthly_multipliers = '1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0'
-    elsif ['base-bldgtype-mf-unit-shared-water-heater-recirc.xml'].include? hpxml_file
+    elsif ['base-bldgtype-mf-unit-shared-water-heater-recirc.xml',
+           'base-bldgtype-mf-unit-shared-water-heater-recirc-scheduled.xml'].include? hpxml_file
       hpxml_bldg.hot_water_distributions[0].has_shared_recirculation = true
       hpxml_bldg.hot_water_distributions[0].shared_recirculation_number_of_units_served = 6
       hpxml_bldg.hot_water_distributions[0].shared_recirculation_pump_power = 220
