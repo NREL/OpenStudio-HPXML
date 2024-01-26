48a1aa5b65f9d9a7e75fff4909febf04c65fe9a5
Scott Horowitz <scott.horowitz@nrel.gov>
2024-01-25 18:19:56 -0700

    Minor cleanup.

--- HPXMLtoOpenStudio/resources/hotwater_appliances.rb
+++ HPXMLtoOpenStudio/resources/hotwater_appliances.rb
@@ -1072,11 +1072,10 @@ class HotWaterAndAppliances
     return f_eff * ref_f_gpd * fixtures_usage_multiplier
   end
 
-  def self.get_water_gains_sens_lat(nbeds, fixtures_usage_multiplier)
+  def self.get_water_gains_sens_lat(nbeds, general_water_use_usage_multiplier = 1.0)
     # Table 4.2.2(3). Internal Gains for Reference Homes
-    fixtures_usage_multiplier = 1.0 if fixtures_usage_multiplier.nil?
-    sens_gains = (-1227.0 - 409.0 * nbeds) * fixtures_usage_multiplier # Btu/day
-    lat_gains = (1245.0 + 415.0 * nbeds) * fixtures_usage_multiplier # Btu/day
+    sens_gains = (-1227.0 - 409.0 * nbeds) * general_water_use_usage_multiplier # Btu/day
+    lat_gains = (1245.0 + 415.0 * nbeds) * general_water_use_usage_multiplier # Btu/day
     return sens_gains * 365.0, lat_gains * 365.0
   end
 
