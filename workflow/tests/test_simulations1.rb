f2f06524c7e83376cab2f840ee45c2257db9596b
Scott Horowitz <scott.horowitz@nrel.gov>
2024-03-29 18:04:54 -0600

    Relax tolerance slightly.

--- workflow/tests/test_simulations1.rb
+++ workflow/tests/test_simulations1.rb
@@ -32,9 +32,9 @@ class WorkflowSimulations1Test < Minitest::Test
       end
     end
     xmls = xmls[0..split_at_file - 1]
-    all_results, all_results_bills = run_simulation_tests(xmls)
+    all_annual_results, all_bill_results = run_simulation_tests(xmls)
 
-    _write_results(all_results.sort_by { |k, _v| k.downcase }.to_h, results_out)
-    _write_results(all_results_bills.sort_by { |k, _v| k.downcase }.to_h, bills_out)
+    _write_results(all_annual_results.sort_by { |k, _v| k.downcase }.to_h, results_out)
+    _write_results(all_bill_results.sort_by { |k, _v| k.downcase }.to_h, bills_out)
   end
 end
