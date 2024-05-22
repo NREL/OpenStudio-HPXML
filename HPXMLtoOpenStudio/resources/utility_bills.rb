4634d89abca4e60b8aece535a7f38919b39b59dc
bpark1327 <benjamin.park@nrel.gov>
2024-05-21 22:00:21 -0600

    use more generic logic for seds_rate

--- HPXMLtoOpenStudio/resources/utility_bills.rb
+++ HPXMLtoOpenStudio/resources/utility_bills.rb
@@ -67,17 +67,16 @@ class UtilityBills
       HPXML::FuelTypeWoodPellets => 'mbtu' # $/MBtu
     }
 
-    CSV.foreach(File.join(File.dirname(__FILE__), '../../ReportUtilityBills/resources/simple_rates/pr_all_update.csv'), :headers => true) do |row|
+    CSV.foreach(File.join(File.dirname(__FILE__), '../../ReportUtilityBills/resources/simple_rates/pr_all_update.csv'), headers: true, skip_blanks: true) do |row|
       next if row['State'].upcase != state_code.upcase # State
       next if row['MSN'].upcase != msn_code_map[fuel_type] # EIA SEDS MSN code
 
       if fuel_type == HPXML::FuelTypeCoal
         seds_rate = row['2007'] # Use 2007 prices for coal. For 2008 forward, EIA assumes there is zero residential sector coal consumption in the United States, and SEDS does not estimate a price.
       else
-        seds_rate = row[-1]
-        seds_rate = row[-2] if seds_rate.nil? # If the rate for the latest year is unavailable, use the rate from the previous year.
+        seds_rate = row.to_h.values.reverse.find { |rate| !rate.nil? && !rate.empty? } # If the rate for the latest year is unavailable, find the last non-nil rate.
       end
-      puts state_code, msn_code_map[fuel_type], seds_rate
+
       seds_rate = UnitConversions.convert(Float(seds_rate), unit_conv[fuel_type], 'mbtu')
 
       return seds_rate
