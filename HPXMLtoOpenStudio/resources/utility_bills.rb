9533acf86e30f559ade77d3cb62fb62a50e16d5e
bpark1327 <benjamin.park@nrel.gov>
2024-05-24 13:19:50 -0600

    Handle the case where no EIA SEDS rate was found

--- HPXMLtoOpenStudio/resources/utility_bills.rb
+++ HPXMLtoOpenStudio/resources/utility_bills.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UtilityBills
-  def self.get_rates_from_eia_data(_runner, state_code, fuel_type, fixed_charge, marginal_rate = nil)
+  def self.get_rates_from_eia_data(runner, state_code, fuel_type, fixed_charge, marginal_rate = nil)
     msn_codes = Constants.StateCodesMap.keys
     msn_codes << 'US'
     return unless msn_codes.include? state_code # Check if the state_code is valid
@@ -13,11 +13,11 @@ class UtilityBills
         # Calculate average rate from user-specified fixed charge, user-specified marginal rate, and EIA data
         average_rate = marginal_rate_to_average_rate(marginal_rate, fixed_charge, household_consumption)
       else
-        average_rate = get_eia_seds_rate(state_code, fuel_type)
+        average_rate = get_eia_seds_rate(runner, state_code, fuel_type)
         marginal_rate = average_rate_to_marginal_rate(average_rate, fixed_charge, household_consumption)
       end
     elsif [HPXML::FuelTypeOil, HPXML::FuelTypePropane, HPXML::FuelTypeCoal, HPXML::FuelTypeWoodCord, HPXML::FuelTypeWoodPellets].include? fuel_type
-      marginal_rate = get_eia_seds_rate(state_code, fuel_type)
+      marginal_rate = get_eia_seds_rate(runner, state_code, fuel_type)
     end
 
     marginal_rate = marginal_rate.round(4) unless marginal_rate.nil?
@@ -47,7 +47,7 @@ class UtilityBills
     return marginal_rate + 12.0 * fixed_charge / household_consumption
   end
 
-  def self.get_eia_seds_rate(state_code, fuel_type)
+  def self.get_eia_seds_rate(runner, state_code, fuel_type)
     msn_code_map = {
       HPXML::FuelTypeElectricity => 'ESRCD',
       HPXML::FuelTypeNaturalGas => 'NGRCD',
@@ -72,8 +72,14 @@ class UtilityBills
       next if row['MSN'].upcase != msn_code_map[fuel_type] # EIA SEDS MSN code
 
       seds_rate = row.to_h.values.reverse.find { |rate| rate.to_f != 0 } # If the rate for the latest year is unavailable, find the last non-nil/non-zero rate.
-
-      seds_rate = UnitConversions.convert(Float(seds_rate), unit_conv[fuel_type], 'mbtu')
+      begin
+        seds_rate = Float(seds_rate)
+      rescue ArgumentError, TypeError
+        seds_rate = 0.0
+        runner.registerWarning("No EIA SEDS rate for #{fuel_type} was found for the state of #{state_code}.") if not runner.nil?
+      end
+      
+      seds_rate = UnitConversions.convert(seds_rate, unit_conv[fuel_type], 'mbtu')
 
       return seds_rate
     end
