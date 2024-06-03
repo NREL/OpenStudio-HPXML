c55bcfb62dada708454d98f93fd03a982581d819
Scott Horowitz <scott.horowitz@nrel.gov>
2024-06-03 13:29:27 -0600

    Test run.

--- workflow/tests/util.rb
+++ workflow/tests/util.rb
@@ -6,7 +6,7 @@ def run_simulation_tests(xmls)
   # Run simulations
   puts "Running #{xmls.size} HPXML files..."
   all_annual_results = {}
-  Parallel.map(xmls, in_threads: Parallel.processor_count) do |xml|
+  Parallel.map(xmls, in_threads: 1) do |xml|
     next if xml.end_with? '-10x.xml'
 
     xml_name = File.basename(xml)
