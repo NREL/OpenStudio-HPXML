f38b82faaef35e3e6fae66114b300ed42c7c1788
Joe Robertson <joseph.robertson@nrel.gov>
2024-06-13 13:04:52 -0700

    Avoid requesting unused meters.

--- ReportUtilityBills/measure.rb
+++ ReportUtilityBills/measure.rb
@@ -204,12 +204,14 @@ class ReportUtilityBills < OpenStudio::Measure::ReportingMeasure
 
     # Has production
     has_pv = @hpxml_buildings.select { |hpxml_bldg| !hpxml_bldg.pv_systems.empty? }.size > 0
+    has_battery = @model.getElectricLoadCenterStorageLiIonNMCBatterys.size > 0 # has modeled battery
 
     # Fuel outputs
     fuels.each do |(fuel_type, is_production), fuel|
       fuel.meters.each do |meter|
         next unless has_fuel[hpxml_fuel_map[fuel_type]]
         next if is_production && !has_pv
+        next if meter.include?('ElectricStorage') && !has_battery # we don't need to request this meter if there isn't a modeled battery
 
         result << OpenStudio::IdfObject.load("Output:Meter,#{meter},monthly;").get
         if fuel_type == FT::Elec && @hpxml_header.utility_bill_scenarios.has_detailed_electric_rates
