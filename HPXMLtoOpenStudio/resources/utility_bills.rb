ac1a10be49f2a2d2b0f60c08a9b2b1e52bc93f96
Joe Robertson <joseph.robertson@nrel.gov>
2024-07-15 15:56:02 -0700

    Add test for when marginal rate provided.

--- HPXMLtoOpenStudio/resources/utility_bills.rb
+++ HPXMLtoOpenStudio/resources/utility_bills.rb
@@ -30,17 +30,25 @@ class UtilityBills
     return unless msn_codes.include? state_code # Check if the state_code is valid
 
     average_rate = nil
+
     if [HPXML::FuelTypeElectricity, HPXML::FuelTypeNaturalGas].include? fuel_type
       household_consumption = get_household_consumption(state_code, fuel_type)
-      if not marginal_rate.nil?
-        # Calculate average rate from user-specified fixed charge, user-specified marginal rate, and EIA data
+    end
+
+    if not marginal_rate.nil?
+      if [HPXML::FuelTypeElectricity, HPXML::FuelTypeNaturalGas].include? fuel_type
+        # Calculate average rate from user-specified marginal rate, user-specified fixed charge, and EIA data
         average_rate = marginal_rate_to_average_rate(marginal_rate, fixed_charge, household_consumption)
-      else
+      elsif [HPXML::FuelTypeOil, HPXML::FuelTypePropane, HPXML::FuelTypeCoal, HPXML::FuelTypeWoodCord, HPXML::FuelTypeWoodPellets].include? fuel_type
+        # Do nothing
+      end
+    else
+      if [HPXML::FuelTypeElectricity, HPXML::FuelTypeNaturalGas].include? fuel_type
         average_rate = get_eia_seds_rate(runner, state_code, fuel_type)
         marginal_rate = average_rate_to_marginal_rate(average_rate, fixed_charge, household_consumption)
+      elsif [HPXML::FuelTypeOil, HPXML::FuelTypePropane, HPXML::FuelTypeCoal, HPXML::FuelTypeWoodCord, HPXML::FuelTypeWoodPellets].include? fuel_type
+        marginal_rate = get_eia_seds_rate(runner, state_code, fuel_type)
       end
-    elsif [HPXML::FuelTypeOil, HPXML::FuelTypePropane, HPXML::FuelTypeCoal, HPXML::FuelTypeWoodCord, HPXML::FuelTypeWoodPellets].include? fuel_type
-      marginal_rate = get_eia_seds_rate(runner, state_code, fuel_type)
     end
 
     marginal_rate = marginal_rate.round(4) unless marginal_rate.nil?
