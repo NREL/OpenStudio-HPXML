169617fb56d5c8ee5c27371e981a9fba16308a4d
Joe Robertson <joseph.robertson@nrel.gov>
2024-08-08 09:34:42 -0700

    Try changing unit multiplier tolerances.

--- workflow/tests/util.rb
+++ workflow/tests/util.rb
@@ -1107,10 +1107,14 @@ def _check_unit_multiplier_results(xml, hpxml_bldg, annual_results_1x, annual_re
       # Check that the unmet hours difference is less than 10 hrs
       abs_delta_tol = 10
       abs_frac_tol = nil
-    elsif key.include?('Unmet Loads:')
-      # Check that the unmet loads difference is less than 10 J/hrs FIXME
+    elsif key.include?('Unmet Loads: Hot Water Shower Time')
+      # Check that the unmet loads difference is less than 10 hrs
       abs_delta_tol = 10
       abs_frac_tol = nil
+    elsif key.include?('Unmet Loads: Hot Water Shower Energy')
+      # Check that the unmet loads difference is less than 20000 Btu or less than 10% # FIXME
+      abs_delta_tol = 20000
+      abs_frac_tol = 0.1
     elsif key.include?('HVAC Capacity:') || key.include?('HVAC Design Load:') || key.include?('HVAC Design Temperature:') || key.include?('Weather:') || key.include?('HVAC Geothermal Loop:')
       # Check that there is no difference
       abs_delta_tol = 0
