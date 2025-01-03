5bdc5548e4ea54640e8c7d438af9a5a3bb4ec35b
Yueyue Zhou <yueyue.zhou@nrel.gov>
2025-01-03 13:33:19 -0700

    update calculate_fan_power_from_curve

--- HPXMLtoOpenStudio/resources/hvac.rb
+++ HPXMLtoOpenStudio/resources/hvac.rb
@@ -2539,13 +2539,17 @@ module HVAC
   # @param hvac_system [HPXML::HeatingSystem or HPXML::CoolingSystem or HPXML::HeatPump] The HPXML HVAC system of interest
   # @return [Double] Fan power at any speed or mode
   def self.calculate_fan_power_from_curve(max_fan_power, fan_ratio, hvac_system)
-    if hvac_system.fan_motor_type == HPXML::HVACFanMotorTypeBPM
+    if hvac_system.fan_motor_type.nil?
+      # For system types that fan_motor_type is not specified, the fan_ratio should be 1
+      fail 'Missing fan motor type for systems where more than one speed is modeled' unless fan_ratio == 1.0
+
+      fan_power = max_fan_power
+    elsif hvac_system.fan_motor_type == HPXML::HVACFanMotorTypeBPM
       # BPM fan
-      index = hvac_system.distribution_system_idref.nil? ? 3 : 2.75
-      fan_power = max_fan_power * (fan_ratio**index)
+      pow = hvac_system.distribution_system_idref.nil? ? 3 : 2.75
+      fan_power = max_fan_power * (fan_ratio**pow)
     else
-      # PSC fan or fan_motor_type = nil
-      # For system types that fan_motor_type is not specified, the fan_ratio is 1 and either equation is fine.
+      # PSC fan
       fan_power = max_fan_power * fan_ratio * (0.3 * fan_ratio + 0.7)
     end
     return fan_power
